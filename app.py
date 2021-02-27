d05a52e7cf9f4d0d30ceb0327b8fc3c49749273e
hilma <hilma.holappa@helsinki.fi>
2021-02-27 19:29:08 +0200

    Added choose tag option to male/female comparison

--- app.py
+++ app.py
@@ -56,7 +56,11 @@ def create_dataframes():
     nn1_MF = nn1_MF.groupby(['Year', 'SenderSex']).mean().reset_index()
     nn1_MF = nn1_MF.drop(['WordCount','SenderSexCount'], axis=1)
     #app.logger.info(nn1_MF)
-    #print(nn1_MF)
+    
+    # Male/female noun ratio per tag
+    tag_MF = df.groupby(['ID', 'Tags', 'Year', 'WordCount', 'SenderSex']).size().to_frame(name = 'SenderSexCount').reset_index()
+    tag_MF['PosCountNorm'] = pos_counts['PosCount']/pos_counts['WordCount']*100
+    
 
     # NN1 tag count per year
     nn1_counts = pos_counts[pos_counts['Tags'] == 'NN1']
@@ -65,10 +69,10 @@ def create_dataframes():
     pos_set = set(df['Tags'])
     pos_list = [{'label':tag, 'value':tag} for tag in pos_set]
 
-    return word_counts, pos_counts, nn1_counts, pos_list, nn1_MF
+    return word_counts, pos_counts, nn1_counts, pos_list, nn1_MF, tag_MF
 
 
-word_counts, pos_counts, nn1_counts, pos_list, nn1_MF = create_dataframes()
+word_counts, pos_counts, nn1_counts, pos_list, nn1_MF, tag_MF = create_dataframes()
 
 wc_fig = px.scatter(word_counts, x="Year", y="WordCount", title='Word count for each letter in corpus')
 fm_fig = px.bar(nn1_MF, x="Year", y="PosCountNorm", color='SenderSex', barmode='group')
@@ -98,9 +102,12 @@ app.layout = html.Div(children=[
     # POS NN1 F/M
     , html.Div(
         children=[
-            dcc.Graph(
-                id='M/F gaph',
-                figure=fm_fig
+            dcc.Graph(id='M/F_barChart',)
+            , dcc.Dropdown(
+                id='F/M_dropdown_1',
+                options=pos_list,
+                value=['NN1'],
+                multi=True
             )])
 
     # POS NN1 F/M with year grouping
@@ -116,6 +123,8 @@ app.layout = html.Div(children=[
                 value=10,
                 style={'display': 'inline-block'}
             )])
+
+    
 
     # POS amount per year
     , html.Div(
@@ -211,6 +220,27 @@ def display_grouped_pos_graphs(value):
         df = df.groupby(['YearGroup', 'SenderSex']).mean().reset_index()
         return px.bar(df, x="YearGroup", y="PosCountNorm", color='SenderSex', barmode='group', title='Dynamically group years')  
 
+@app.callback(
+    Output('M/F_barChart', 'figure'), [Input('F/M_dropdown_1', 'value')])
+
+def display_multiple_tags_barchart(values):
+    if values is None:
+        raise PreventUpdate
+    else:
+        mask = tag_MF['Tags'].isin(values)
+        fig= px.bar(
+            # can choose only one tag at a time
+            data_frame=tag_MF[mask].groupby(['Year', 'SenderSex']).mean().reset_index(),
+            x='Year', 
+            y='PosCountNorm',
+            range_y=[0,30],
+            labels={
+                'Year': 'Year', 
+                'PosCountNorm':'Percentage of Tag'},
+            color='SenderSex',
+            barmode='group',
+            title='Compare male and female tags')
+        return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
