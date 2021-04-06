40dbce28fc8d9d41bd46e95f4cc8e7ff0573c061
miiaramo <miia.ramo@icloud.com>
2021-04-06 14:15:02 +0300

    New build

--- layout_tm.py
+++ layout_tm.py
@@ -4,13 +4,9 @@ import dash_bootstrap_components as dbc
 import dash_daq as daq
 import dash_table
 import pandas as pd
-<<<<<<< HEAD
-from app import data_parser
-=======
 import globals
 
 data_parser = globals.data_parser
->>>>>>> main
 
 layout2 = html.Div([
     html.Nav(
