4a25708ded49e1b099b6500b81faab9a5a420451
hilma <hilma.holappa@helsinki.fi>
2021-04-09 13:46:34 +0300

    YearGroups and tag selection to main bar chart

--- callbacks_pos.py
+++ callbacks_pos.py
@@ -93,6 +93,62 @@ def display_line_graph(n_clicks, values0, values1, values2, values3, periods):
 
 
 @app.callback(
+    Output('pos_groups_dropdown_bar1_sub', 'value'),
+    Output('pos_groups_dropdown_bar1_sub', 'options'),
+    [Input('pos_groups_dropdown_bar1_main', 'value')])
+def bar_tag_options(mains):
+
+    values = []
+    options = []
+    for main in mains:
+        value = list(data_parser.pos_categories[main].keys())
+        values.extend(value)
+        options.extend(data_parser.list_to_dash_option_dict(value))
+    
+    return values, options
+
+
+@app.callback(
+    Output('bar_chart', 'figure'), 
+    Input('update_bar_button', 'n_clicks'), # Only pressing the button initiates the function
+    [State('pos_groups_dropdown_bar1_main', 'value')],
+    [State('pos_groups_dropdown_bar1_sub', 'value')],
+    [State('year-group-number-bar', 'value')])
+def display_bar_chart(n_clicks, values0, values1, periods):
+
+    if n_clicks is not None:
+        bins = pd.interval_range(start=1680, end=1800, periods=periods, closed='right')
+        labels = list(bins.astype(str))
+
+        df = data_parser.df
+        df = df.groupby(['ID', 'SenderSex', 'SenderRank', 'RelCode', 'Tags', 'Year', 'WordCount']).size().to_frame(name = 'PosCount').reset_index()
+        df['PosCountNorm'] = df['PosCount']/df['WordCount']*100
+        
+        df['Year'] = df['Year'].astype('int')
+        df['YearGroup'] = pd.cut(df['Year'], bins=bins,include_lowest=True, labels=labels, precision=0)
+        df['YearGroup'] = df['YearGroup'].astype("str")
+        df = df.groupby(['YearGroup', 'Tags', 'SenderSex']).mean().reset_index()
+
+        fig = go.Figure()
+        mask = df['Tags'].isin(values1)
+        fig= px.bar(
+            data_frame=df[mask].groupby(['Tags', 'YearGroup', 'SenderSex']).mean().reset_index(),
+            x='YearGroup', 
+            y='PosCountNorm',
+            range_y=[0,30],
+            labels={
+                'YearGroup': 'Year', 
+                'PosCountNorm':'Percentage'},
+            hover_data=['Tags'],
+            color='SenderSex',
+            barmode='group',
+            title='main bar chart')
+
+        fig.update_layout(yaxis_range=[0,50])
+
+        return fig
+
+@app.callback(
     Output('m-f-graph-year-grouping', 'figure'), 
     [Input('year-group-number', 'value')])
 def display_grouped_pos_graphs1(value):
--- layout_pos.py
+++ layout_pos.py
@@ -32,6 +32,37 @@ layout1 = html.Div([
             ),
             dcc.Tab(label='Bar', 
                 children=[
+                    # main bar chart
+                    html.Div(
+                        children=[
+                            dcc.Graph(id='bar_chart'),
+                            "Select the number of year groups",
+                            html.Br(),
+                            dcc.Input(
+                                id="year-group-number-bar", 
+                                type="number", 
+                                placeholder="input number of groups",
+                                value=10
+                            ),
+                            html.Hr(),
+                            'Tag selection',
+                            html.Br(),
+                            dcc.Dropdown(
+                                id='pos_groups_dropdown_bar1_main',
+                                options=data_parser.list_to_dash_option_dict(list(data_parser.pos_categories.keys())), 
+                                value=['nouns'],
+                                multi=True
+                            ),
+                            dcc.Dropdown(
+                                id='pos_groups_dropdown_bar1_sub',
+                                options=data_parser.list_to_dash_option_dict(list(data_parser.pos_categories['nouns'].keys())), 
+                                value=list(data_parser.pos_categories['nouns'].keys()),
+                                multi=True
+                            ),
+                            html.Br(), 
+                            html.Button('Apply selection', id='update_bar_button', n_clicks = 0)
+                        ]
+                    ),
                     # POS NN1 F/M
                     html.Div(
                         children=[
