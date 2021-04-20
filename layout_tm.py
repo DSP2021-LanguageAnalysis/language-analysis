4da24916f6953237b31704b2facac063c49d040c
arthurhughes <71314304+arthurhughes@users.noreply.github.com>
2021-04-20 20:50:18 +0300

    Update layout_tm.py

--- layout_tm.py
+++ layout_tm.py
@@ -15,6 +15,7 @@ layout2 = html.Div(
         className ='navbar navbar-expand-lg navbar-dark bg-primary', 
         children=[
             html.H1(className='navbar-brand', children='VARIENG: TCEECE corpus analysis'),
+            dbc.NavItem(dbc.NavLink('Overview', href='/app/overview')),
             dbc.NavItem(dbc.NavLink('POS tag analysis', href='/app/postags')),
             dbc.NavItem(dbc.NavLink('Topic model', active=True, href='/app/topicmodel')),
             dbc.NavItem(dbc.NavLink('Add custom groups', href='/app/customize'))
